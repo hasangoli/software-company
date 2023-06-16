7e80e57b636310c0267839d7e875adcafa4c1e82
Hasan <mrhasangoli@gmail.com>
2023-06-16 20:30:48 +0330

    Updated!

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import Image from 'next/image';
-
 export default function Home() {
-  return (
-    <main>
-      <div className='relative w-full aspect-[16/9]'>
-        <Image src='/assets/images/hero.png' alt='hero' fill priority />
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
